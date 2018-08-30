24627d15a26b0aea4363719381c81b4d80d7aa91
Roger Romero <rromero@ncl.com>
2018-08-30 18:33:16 -0400

    run gulp build

--- dist/jquery.bxslider.js
+++ dist/jquery.bxslider.js
@@ -1101,12 +1101,16 @@
      *  - DOM event object
      */
     var onTouchStart = function(e) {
+      var parentHorizontalPadding;
       // watch only for left mouse, touch contact and pen contact
       // touchstart event object doesn`t have button property
       if (e.type !== 'touchstart' && e.button !== 0) {
         return;
       }
-      e.preventDefault();
+
+      //Commenting the line below prevents the scroll issue when touching over the slider on mobile
+      //e.preventDefault();
+
       //disable slider controls while user is interacting with slides to avoid slider freeze that happens on touch devices when a slide swipe happens immediately after interacting with slider controls
       slider.controls.el.addClass('disabled');
 
@@ -1115,6 +1119,12 @@
       } else {
         // record the original position when touch starts
         slider.touch.originalPos = el.position();
+        parentHorizontalPadding = slider.viewport.innerWidth() - slider.viewport.width();
+
+        // In case slider parent have horizontal padding recalculate position
+        if(parentHorizontalPadding > 0)
+            slider.touch.originalPos.left -= (parentHorizontalPadding/2);
+
         var orig = e.originalEvent,
         touchPoints = (typeof orig.changedTouches !== 'undefined') ? orig.changedTouches : [orig];
 		var chromePointerEvents = typeof PointerEvent === 'function'; 
